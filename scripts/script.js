8641171d08f14917670b96a0bf22825b63bb3581
Sally Lei <sillyslei@gmail.com>
2021-02-14 14:51:59 -0800

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,10 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
-  if(localStorage.getItem("products")===null){
-    fetch("https://fakestoreapi.com/products")
-      .then(response => response.json())
-      .then(data => localStorage.setItem("products", JSON.stringify(data)));
-  }
+  fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => localStorage.setItem("products", JSON.stringify(data)));
+  
   var storedProducts = JSON.parse(localStorage.getItem("products"));  
   var productList = document.getElementById("product-list");
 
